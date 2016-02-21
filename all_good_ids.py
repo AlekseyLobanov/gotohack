591478a2c008eef9a8777568867afe7c5fd416f1
Aleksey Lobanov <alekseylobanov1@gmail.com>
2016-02-21 13:06:52 +0300

    We build json with freqs of ids

--- all_good_ids.py
+++ all_good_ids.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*- 
 
 import sys
+import json
 import os
 from os.path import join
 
-all_ids = set()
+all_ids = {}
 
 for dirpath, dirnames, filenames in os.walk(sys.argv[1]):
     for f in filenames:
         fp = os.path.join(dirpath, f)
         for uid in open(fp):
-            all_ids.add(int(uid))
+            uid = int(uid)
+            if uid in all_ids:
+                all_ids[uid] += 1
+            else:
+                all_ids[uid] = 1
 
 f_out = open(sys.argv[2],'w')
-for uid in all_ids:
-    f_out.write(str(uid) + '\n')
+f_out.write(json.dumps(all_ids))
